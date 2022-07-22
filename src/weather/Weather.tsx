c706b2a0869249b3ee03a516a6e1081384bc61fa
Avantgarde95 <mathematicianscott@gmail.com>
2022-07-22 21:30:30 +0900

    [feat] Implement the components
    * DateView
      * I named it to "DateView" to distinguish it from JS Date
    * Location
    * State
    * Icon
    * Temperature

--- src/weather/Weather.tsx
+++ src/weather/Weather.tsx
@@ -1,13 +1,58 @@
 import React from "react";
 import styled from "styled-components";
 
-const Weather = () => <Container>Hello</Container>;
+const Weather = () => (
+  <Container isDay>
+    <DateView>9/30 SAT</DateView>
+    <Location>Seoul</Location>
+    <State>Rain</State>
+    <Icon src="http://openweathermap.org/img/wn/10d@4x.png" alt="10d" />
+    <Temperature>10&deg;C</Temperature>
+  </Container>
+);
 
-const Container = styled.div`
+interface ContainerProps {
+  isDay: boolean;
+}
+
+const Container = styled.div<ContainerProps>`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
   width: 100%;
   height: 100%;
 
   font-size: 2rem;
+
+  color: #ffffff;
+  background-color: ${({ isDay }) => (isDay ? "#a2cbe4" : "#030176")};
+`;
+
+const DateView = styled.div`
+  margin-bottom: 0.5rem;
+  font-size: 2rem;
+`;
+
+const Location = styled.div`
+  margin-bottom: 0.5rem;
+  font-size: 4rem;
+  font-weight: bold;
+`;
+
+const State = styled.div`
+  margin-bottom: 2rem;
+  font-size: 3rem;
+`;
+
+const Icon = styled.img`
+  margin-bottom: 2rem;
+`;
+
+const Temperature = styled.div`
+  font-size: 6rem;
+  font-weight: bold;
 `;
 
 export default Weather;
